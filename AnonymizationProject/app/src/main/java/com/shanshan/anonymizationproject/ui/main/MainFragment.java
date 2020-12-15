b246483f1f014e92c40b31047ecfb0f5a9dafbcf
ShanY-33 <yinnn33@gmail.com>
2020-12-15 23:01:22 +0100

    create app

---
+++ AnonymizationProject/app/src/main/java/com/shanshan/anonymizationproject/ui/main/MainFragment.java
@@ -0,0 +1,39 @@
+package com.shanshan.anonymizationproject.ui.main;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.shanshan.anonymizationproject.R;
+
+public class MainFragment extends Fragment {
+
+    private MainViewModel mViewModel;
+
+    public static MainFragment newInstance() {
+        return new MainFragment();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.main_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(MainViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ AnonymizationProject/app/src/main/java/com/shanshan/anonymizationproject/ui/main/MainViewModel.java
@@ -0,0 +1,7 @@
+package com.shanshan.anonymizationproject.ui.main;
+
+import androidx.lifecycle.ViewModel;
+
+public class MainViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
