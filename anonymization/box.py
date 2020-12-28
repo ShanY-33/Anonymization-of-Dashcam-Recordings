e6a80d5c68153b61f0f5376a6c076c4b5c9c47b9
ShanY-33 <yinnn33@gmail.com>
2020-12-28 22:02:29 +0100

    update

---
+++ anonymization/box.py
@@ -0,0 +1,6 @@
+class Box():
+    def __init__(self, box_dict):
+        self.box_dict = box_dict
+
+
+
---
+++ anonymization/utils/convert_coordinate.py
@@ -0,0 +1,31 @@
+import numpy as np
+
+
+def rel_to_abs(img_h: int, img_w: int,  rel_box):
+    abs_box = rel_box.copy()
+    abs_box[:, 0:: 2] = img_h * rel_box[:, 0:: 2]
+    abs_box[:, 1:: 2] = img_w * rel_box[:, 1:: 2]
+    abs_box = abs_box.astype(int)
+    return abs_box
+
+
+def abs_to_rel(img_h: int, img_w: int, abs_box):
+    rel_box = abs_box.copy()
+    rel_box[:, 0:: 2] = rel_box[:, 0:: 2] / float(img_h)
+    rel_box[:, 1:: 2] = rel_box[:, 1:: 2] / float(img_w)
+    return rel_box
+
+
+def xyxy_to_xyhw(abs_box):
+    # (xmin, ymin, height, width)
+    abs_box[:, 2] = abs_box[:, 2] - abs_box[:, 0]
+    abs_box[:, 3] = abs_box[:, 3] - abs_box[:, 1]
+    return abs_box
+
+
+def xyhw_to_xyxy(abs_box):
+    # (xmin, ymin, xmax, ymax)
+    abs_box[:, 2] = abs_box[:, 2] + abs_box[:, 0]
+    abs_box[:, 3] = abs_box[:, 3] + abs_box[:, 1]
+    return abs_box
+
