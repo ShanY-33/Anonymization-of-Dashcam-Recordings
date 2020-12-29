be0b5baef0056ae9373423853f66f8cd77a4b117
ShanY-33 <yinnn33@gmail.com>
2020-12-29 16:45:52 +0100

    update

--- anonymization/image_processing.py
+++ anonymization/image_processing.py
@@ -1,4 +1,4 @@
-import utils
+from anonymization.utils import save_load
 import numpy as np
 from PIL import Image
 import tensorflow as tf
@@ -22,54 +22,57 @@ class Img():
         self.image_np = np.asarray(Image.open(image_path))
         self.height = self.image_np.shape[0]
         self.width = self.image_np.shape[1]
-        self.boxes = []
+        self.boxes_list = []  # boxes_list[0] --> car & person box, boxes_list[0] --> merged box, boxes_list[>1] --> subimage boxes
         self.subimg = []
 
+    def detection_all_classes(self, model, threshold):
+        image = np.asarray(self.image_np)
+        # The input needs to be a tensor, convert it using `tf.convert_to_tensor`.
+        input_tensor = tf.convert_to_tensor(image)
+        # The model expects a batch of images, so add an axis with `tf.newaxis`.
+        input_tensor = input_tensor[tf.newaxis, ...]
 
-def detection_all_classes(self, model, threshold):
-    image = np.asarray(self.image_np)
-    # The input needs to be a tensor, convert it using `tf.convert_to_tensor`.
-    input_tensor = tf.convert_to_tensor(image)
-    # The model expects a batch of images, so add an axis with `tf.newaxis`.
-    input_tensor = input_tensor[tf.newaxis, ...]
+        # Run inference
+        model_fn = model.signatures['serving_default']
+        output_dict = model_fn(input_tensor)
 
-    # Run inference
-    model_fn = model.signatures['serving_default']
-    output_dict = model_fn(input_tensor)
+        # All outputs are batches tensors.
+        # Convert to numpy arrays, and take index [0] to remove the batch dimension.
+        # We're only interested in the first num_detections.
+        num_detections = int(output_dict.pop('num_detections'))
+        output_dict = {key: value[0, :num_detections].numpy() for key, value in output_dict.items()}
+        output_dict['num_detections'] = num_detections
 
-    # All outputs are batches tensors.
-    # Convert to numpy arrays, and take index [0] to remove the batch dimension.
-    # We're only interested in the first num_detections.
-    num_detections = int(output_dict.pop('num_detections'))
-    output_dict = {key: value[0, :num_detections].numpy() for key, value in output_dict.items()}
-    output_dict['num_detections'] = num_detections
-
-    # detection_classes should be ints.
-    output_dict['detection_classes'] = output_dict['detection_classes'].astype(np.int64)
-    
-    # Handle models with masks:
-    if 'detection_masks' in output_dict:
-        # Reframe the the bbox mask to the image size.
-        detection_masks_reframed = utils_ops.reframe_box_masks_to_image_masks(
-                output_dict['detection_masks'], output_dict['detection_boxes'],
-                image.shape[0], image.shape[1])      
-        detection_masks_reframed = tf.cast(detection_masks_reframed > threshold, tf.uint8)
-        output_dict['detection_masks_reframed'] = detection_masks_reframed.numpy()
+        # detection_classes should be ints.
+        output_dict['detection_classes'] = output_dict['detection_classes'].astype(np.int64)
         
-    return(output_dict)
+        # Handle models with masks:
+        if 'detection_masks' in output_dict:
+            # Reframe the the bbox mask to the image size.
+            detection_masks_reframed = utils_ops.reframe_box_masks_to_image_masks(
+                    output_dict['detection_masks'], output_dict['detection_boxes'],
+                    image.shape[0], image.shape[1])      
+            detection_masks_reframed = tf.cast(detection_masks_reframed > threshold, tf.uint8)
+            output_dict['detection_masks_reframed'] = detection_masks_reframed.numpy()
+            
+        return(output_dict)
 
+    def detection_car_person(self, model, threshold=THRESHOLD_CAR_PERSON, detection_classes=(1, 3, 4)):
+        # 1: person; 3: car; 4: motorcycle; 6: bus; 8: truck
+        output_dict = self.detection_all_classes(model, threshold)
+        len = output_dict['detection_boxes'].shape[0]
+        output_dict_list = output_dict.copy()
+        output_dict_list['detection_boxes'] = (output_dict_list['detection_boxes']).tolist()
+        output_dict_list['detection_classes'] =(output_dict_list['detection_classes']).tolist()
+        output_dict_list['detection_scores'] = (output_dict_list['detection_scores']).tolist()
 
-def detection_car_person(self, model, threshold=THRESHOLD_CAR_PERSON, detection_classes=(1, 3, 4)):
-    # 1: person; 3: car; 4: motorcycle; 6: bus; 8: truck
-    output_dict = self.detection_all_classes(model, threshold)
-    boxes = output_dict['detection_boxes']
-    dict_car_person = []
+        for i in range(len-1, -1, -1):
+            if (output_dict['detection_classes'][i] in detection_classes):
+                del (output_dict_list['detection_boxes'])[i]
+                del (output_dict_list['detection_classes'])[i]
+                del (output_dict_list['detection_scores'])[i]
 
-    for i in range(boxes.shape[0]):
-        if (output_dict['detection_classes'][i] in detection_classes):
-            dict_car_person.append(output_dict[i])
-
-    self.boxes.append(dict_car_person)
+        self.boxes_list.append(output_dict)
 
 
 
--- detection.py
+++ detection.py
@@ -1,16 +1,31 @@
-from anonymization.image_processing import Img
+# import anonymization
 from anonymization import utils
-import anonymization
+from anonymization.image_processing import Img
 
+from object_detection.utils import ops as utils_ops
+from object_detection.utils import label_map_util
+from object_detection.utils import visualization_utils as vis_util
 
-img_dir = '../res/testimg/input'
-model_dir = '../res/model/saved_model'
-label_path = '../res/model/config/label_map.pbtxt'
+import tensorflow as tf
+
+# patch tf1 into `utils.ops`
+utils_ops.tf = tf.compat.v1
+
+# Patch the location of gfile
+tf.gfile = tf.io.gfile
+
+img_dir = '/home/shanshan/github/Anonymization-of-Dashcam-Recordings/res/testimg/input'
+model_dir = '/home/shanshan/github/Anonymization-of-Dashcam-Recordings/res/model/saved_model'
+label_path = '/home/shanshan/github/Anonymization-of-Dashcam-Recordings/res/model/config/label_map.pbtxt'
+
 
 TEST_IMAGE_PATHS = utils.save_load.get_image_paths(img_dir)
 
-model = utils.load_model(model_dir)
-label = utils.load_label(label_path)
+model = utils.save_load.load_model(model_dir)
+label = utils.save_load.load_label(label_path)
+
 
 for img_path in TEST_IMAGE_PATHS:
     input_img = Img(img_path)
+    input_img.detection_car_person(model)
+    print(input_img.boxes_list[0]['detection_boxes'])  # car and person
