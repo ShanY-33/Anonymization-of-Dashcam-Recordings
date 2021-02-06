fb2a9cbb66f2f14e2d715134cc5bf746b03b9f25
ShanY-33 <yinnn33@gmail.com>
2021-02-06 19:05:15 +0100

    add crop bitmap

--- android-demo/Anonymization/app/src/main/java/com/example/anonymization/Imgprocess.java
+++ android-demo/Anonymization/app/src/main/java/com/example/anonymization/Imgprocess.java
@@ -55,4 +55,13 @@ public class Imgprocess {
         overlayView.setRecognitions(recognitions);
         overlayView.postInvalidate();
     }
+
+    protected Bitmap CropImg(Bitmap bitmap, Recognition recognition) {
+        int imageSizeX = bitmap.getWidth(), imageSizeY = bitmap.getHeight();
+        int left = Math.round(recognition.getLocation().left * imageSizeX);
+        int top = Math.round(recognition.getLocation().top * imageSizeY);
+        int right = Math.round(recognition.getLocation().right * imageSizeX);
+        int bottom = Math.round(recognition.getLocation().bottom * imageSizeY);
+        return (Bitmap.createBitmap(bitmap, left, top, right - left, bottom - top));
+    }
 }
